91eb6a3bd410817722ac02626d3f728b0ffcb966
ganesh <ganeshguru505@gmail.com>
2026-01-15 20:50:53 +0530

    new

--- components/Oauth2Button.tsx
+++ components/Oauth2Button.tsx
@@ -5,10 +5,10 @@ import Link from 'next/link'
 const Oauth2Button = () => {
   return (
      <div className='flex flex-col gap-4 '>
-           <Link href={"http://localhost:8080/oauth2/authorization/google"}>
+           <Link href={`${process.env.NEXT_PUBLIC_API_BASE_URL}/oauth2/authorization/google`}>
             <Button type='button' className=' text-white bg-gray-500 cursor-pointer w-full'><i className="ri-google-fill mr-2"></i>Google</Button>
            </Link>
-           <Link href={"http://localhost:8080/oauth2/authorization/github"} >
+           <Link href={`${process.env.NEXT_PUBLIC_API_BASE_URL}/oauth2/authorization/github`} >
             <Button type='button' className=' text-white bg-gray-600 cursor-pointer w-full'><i className="ri-github-fill mr-2"></i>Github</Button>
            </Link>
           </div>
