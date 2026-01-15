039fa606022da23af74169a56f36f315eaafd7d3
ganesh <ganeshguru505@gmail.com>
2026-01-15 21:23:46 +0530

    chnage

--- components/Oauth2Button.tsx
+++ components/Oauth2Button.tsx
@@ -3,14 +3,22 @@ import { Button } from './ui/button'
 import Link from 'next/link'
 
 const Oauth2Button = () => {
+
+  const googleLogin=()=>{
+    window.location.href=`${process.env.NEXT_PUBLIC_API_BASE_URL}/oauth2/authorization/google`
+  }
+  const githubLogin=()=>{
+    window.location.href=`${process.env.NEXT_PUBLIC_API_BASE_URL}/oauth2/authorization/github`
+  }
+
   return (
      <div className='flex flex-col gap-4 '>
-           <Link href={`${process.env.NEXT_PUBLIC_API_BASE_URL}/oauth2/authorization/google`}>
-            <Button type='button' className=' text-white bg-gray-500 cursor-pointer w-full'><i className="ri-google-fill mr-2"></i>Google</Button>
-           </Link>
-           <Link href={`${process.env.NEXT_PUBLIC_API_BASE_URL}/oauth2/authorization/github`} >
-            <Button type='button' className=' text-white bg-gray-600 cursor-pointer w-full'><i className="ri-github-fill mr-2"></i>Github</Button>
-           </Link>
+           
+            <Button onClick={googleLogin} type='button' className=' text-white bg-gray-500 cursor-pointer w-full'><i className="ri-google-fill mr-2"></i>Google</Button>
+            
+            
+            <Button onClick={githubLogin} type='button' className=' text-white bg-gray-600 cursor-pointer w-full'><i className="ri-github-fill mr-2"></i>Github</Button>
+           
           </div>
   )
 }
